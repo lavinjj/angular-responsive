759e0808b4235e7895f403faa48c300fdd6e9fdf
Thomas Burleson <ThomasBurleson@Gmail.com>
2014-04-25 18:17:29 -0500

    Refactored responsive-directive.js
    Refactored for DRY usages
    Fixed memory leak with watchers
    Optimized checks for window width and smartDevice

--- src/responsive-directive.js
+++ src/responsive-directive.js
@@ -1,169 +1,147 @@
-(function () {
+(function ( angular ) {
     'use strict';
 
-    angular.module('angular-responsive', [])
-        // localization service responsible for retrieving resource files from the server and
-        // managing the translation dictionary
-        .provider('responsiveHelper', [function () {
-            var injector = angular.injector(['ng']);
-            var $window = injector.get('$window');
+    angular
+        .module('angular-responsive', [])
+        .provider('responsiveHelper', ["$windowProvider", function ($windowProvider)
+        {
+            // Gather winWidth and smartDevice information only 1x at app startup...
 
-            var helper = {
+            var $window  = $windowProvider.$get();
+            var winWidth = $window['outerWidth'];
+            var helper   = {
 
-                isSmartDevice: function () {
-                    // Adapted from http://www.detectmobilebrowsers.com
-                    var ua = $window['navigator']['userAgent'] || $window['navigator']['vendor'] || $window['opera'];
+                isSmartDevice : isSmartDevice( $window ),
+                isMobile      : function () { return helper.isSmartDevice && (winWidth <= 767);   },
+                isTablet      : function () { return helper.isSmartDevice && (winWidth > 767);    },
+                isDesktop     : function () { return !helper.isSmartDevice;                       }
 
-                    // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile devices
-                    return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
-                },
-
-                isMobile: function () {
-                    var width = $window['outerWidth'];
-
-                    var smartDevice = helper.isSmartDevice();
-
-                    return smartDevice && width <= 767;
-                },
-
-                isTablet: function () {
-                    var width = $window['outerWidth'];
-
-                    var smartDevice = helper.isSmartDevice();
-
-                    return smartDevice && width >= 768;
-                },
-
-                isDesktop: function () {
-                    return !helper.isSmartDevice();
-                }
             };
+
+            // Publish accessor function...
 
             this.$get = function() {
                 return helper;
             };
-        } ])
-
-        .directive('arMobile', ['responsiveHelper', function (responsiveHelper) {
-            return {
-                restrict: "EAC",
-                transclude: 'element',
-                template: '<div></div>',
-                compile: function (element, attr, transclude) {
-                    return function postLink(scope, element, attr) {
-                        var childElement, childScope;
-                        scope.$watch(attr['arMobile'], function () {
-                            if (childElement) {
-                                childElement.remove();
-                                childScope.$destroy();
-                                childElement = undefined;
-                                childScope = undefined;
-                            }
-                            if (responsiveHelper.isMobile()) {
-                                childScope = scope.$new();
-                                childElement = transclude(childScope, function (clone) {
-                                    element.after(clone);
-                                });
-                            }
-                        });
-                    }
-                }};
         }])
 
-        .directive('arTablet', ['responsiveHelper', function (responsiveHelper) {
+        /**
+         * Is this a limited-display-size mobile device
+         */
+        .directive('arMobile', ['responsiveHelper', function (responsiveHelper)
+        {
             return {
-                restrict: "EAC",
-                transclude: 'element',
-                template: '<div></div>',
-                compile: function (element, attr, transclude) {
-                    return function postLink(scope, element, attr) {
-                        var childElement, childScope;
-                        scope.$watch(attr['arTablet'], function () {
-                            if (childElement) {
-                                childElement.remove();
-                                childScope.$destroy();
-                                childElement = undefined;
-                                childScope = undefined;
-                            }
-                            if (responsiveHelper.isTablet()) {
-                                childScope = scope.$new();
-                                childElement = transclude(childScope, function (clone) {
-                                    element.after(clone);
-                                });
-                            }
-                        });
-                    }
-                }};
-        }])
-
-        .directive('arDesktop', ['responsiveHelper', function (responsiveHelper) {
-            return {
-                restrict: "EAC",
-                transclude: 'element',
-                template: '<div></div>',
-                compile: function (element, attr, transclude) {
-                    return function postLink(scope, element, attr) {
-                        var childElement, childScope;
-                        scope.$watch(attr['arDesktop'], function () {
-                            if (childElement) {
-                                childElement.remove();
-                                childScope.$destroy();
-                                childElement = undefined;
-                                childScope = undefined;
-                            }
-                            if (responsiveHelper.isDesktop()) {
-                                childScope = scope.$new();
-                                childElement = transclude(childScope, function (clone) {
-                                    element.after(clone);
-                                });
-                            }
-                        });
-                    }
-                }};
-        }])
-
-        .directive('arResponsive', ['responsiveHelper', function (responsiveHelper) {
-            return {
-                restrict: "EAC",
-                transclude: 'element',
-                template: '<div></div>',
-                compile: function (element, attr, transclude) {
-                    return function postLink(scope, element, attr) {
-                        var deviceTypes = scope.$eval(attr['arResponsive']);
-                        var childElement, childScope;
-
-                        scope.$watch(deviceTypes, function () {
-                                if (childElement) {
-                                    childElement.remove();
-                                    childScope.$destroy();
-                                    childElement = undefined;
-                                    childScope = undefined;
-                                }
-
-                                var showElement = false;
-
-                                if (!showElement && deviceTypes['Mobile']) {
-                                    showElement = responsiveHelper.isMobile();
-                                }
-                                if (!showElement && deviceTypes['Tablet']) {
-                                    showElement = responsiveHelper.isTablet();
-                                }
-                                if (!showElement && deviceTypes['Desktop']) {
-                                    showElement = responsiveHelper.isDesktop();
-                                }
-
-                                if (showElement) {
-                                    childScope = scope.$new();
-                                    childElement = transclude(childScope, function (clone) {
-                                        element.after(clone);
-                                    });
-                                }
-                            }
-                        );
-                    }
-                }
+                restrict    : "EAC",
+                transclude  : 'element',
+                template    : '<div></div>',
+                compile     : buildCompileFn( 'arMobile', responsiveHelper.isMobile )
             };
-        }
-        ])
+        }])
 
-})();
+        /**
+         * Is this a table device
+         */
+        .directive('arTablet', ['responsiveHelper', function (responsiveHelper)
+        {
+            return {
+                restrict    : "EAC",
+                transclude  : 'element',
+                template    : '<div></div>',
+                compile     : buildCompileFn( 'arTablet', responsiveHelper.isTablet )
+            };
+        }])
+
+        /**
+         * Is this a desktop device
+         */
+        .directive('arDesktop', ['responsiveHelper', function (responsiveHelper)
+        {
+            return {
+                restrict    : "EAC",
+                transclude  : 'element',
+                template    : '<div></div>',
+                compile     : buildCompileFn( 'arDesktop', responsiveHelper.isDesktop )
+            };
+        }])
+
+        /**
+         * Does the device a match user-specified combination (0..3) of device types
+         */
+        .directive('arResponsive', ['responsiveHelper', function (responsiveHelper)
+        {
+            return {
+                restrict    : "EAC",
+                transclude  : 'element',
+                template    : '<div></div>',
+                compile     : buildCompileFn( 'arResponsive', checkAllTypes(responsiveHelper) )
+            };
+        }]);
+
+    /**
+     * Partial application for DRY construction of a directive compile function
+     */
+    function buildCompileFn(responsiveType, verifyFn )
+    {
+        return function compile(element, attr, transclude)
+        {
+            return function postLink(scope, element, attr)
+            {
+                var childElement, childScope,
+                    config  = scope.$eval( attr[responsiveType] ),
+                    unwatch = scope.$watch( config, function ()
+                    {
+                        // attribute changed, delete existing element & $scope
+
+                        if (childElement) {
+                            childElement.remove();
+                            childScope.$destroy();
+                            childElement = undefined;
+                            childScope = undefined;
+                        }
+
+                        if ( verifyFn(config) )
+                        {
+                            // Create a new element and $scope...
+
+                            childScope = scope.$new();
+                            childElement = transclude(childScope, function (clone) {
+                                element.after(clone);
+                            });
+                        }
+                    });
+
+                // Fix memory leak an remove watcher when element/directive is released
+
+                scope.$on( "$destroy", unwatch );
+            };
+        };
+    }
+
+    /**
+     * Partial application for DRY construction of function to scan of any valid responsive types
+     */
+    function checkAllTypes(responsiveHelper)
+    {
+        return function( deviceTypes )
+        {
+            return  ( deviceTypes['Mobile']  && responsiveHelper.isMobile()  ) ||
+                ( deviceTypes['Tablet']  && responsiveHelper.isTablet()  ) ||
+                ( deviceTypes['Desktop'] && responsiveHelper.isDesktop() ) || false;
+        };
+    }
+
+    /**
+     * Scan to determine if current window is hosted within a `smart` device
+     * @param $window
+     * @returns {boolean}
+     */
+    function isSmartDevice( $window )
+    {
+        // Adapted from http://www.detectmobilebrowsers.com
+        var ua = $window['navigator']['userAgent'] || $window['navigator']['vendor'] || $window['opera'];
+
+        // Checks for iOs, Android, Blackberry, Opera Mini, and Windows mobile devices
+        return (/iPhone|iPod|iPad|Silk|Android|BlackBerry|Opera Mini|IEMobile/).test(ua);
+    }
+
+})( window.angular );
